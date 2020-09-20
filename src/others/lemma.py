294f60582f0b13c1d67b271454efff21817a5537
MichQiu <qiu_michael@hotmail.com>
2020-09-20 10:27:05 +0100

    Changed optional pattern matching from zero or more to one or more

--- src/others/lemma.py
+++ src/others/lemma.py
@@ -24,41 +24,41 @@ with open("/home/mich_qiu/PycharmProjects/MSc_Thesis/Bug-Report-Summarization/sr
                 if token.pos_ == pos:
                     pos_other = deepcopy(pos_lst)
                     pos_other.pop(idx)
-                    heur_sent.append({"POS": token.pos_, "OP": "*"})
-                    heur_sent2.append({"POS": pos_other[0], "OP": "*"})
-                    heur_sent3.append({"POS": pos_other[-1], "OP": "*"})
+                    heur_sent.append({"POS": token.pos_, "OP": "+"})
+                    heur_sent2.append({"POS": pos_other[0], "OP": "+"})
+                    heur_sent3.append({"POS": pos_other[-1], "OP": "+"})
             if heur_sent2:
                 if token.text == "verb":
-                    heur_sent.append({"POS": "VERB", "OP": "*"})
-                    heur_sent2.append({"POS": "VERB", "OP": "*"})
-                    heur_sent3.append({"POS": "VERB", "OP": "*"})
+                    heur_sent.append({"POS": "VERB", "OP": "+"})
+                    heur_sent2.append({"POS": "VERB", "OP": "+"})
+                    heur_sent3.append({"POS": "VERB", "OP": "+"})
                 elif token.text == "modal":
-                    heur_sent.append({"POS": "AUX", "OP": "*"})
-                    heur_sent2.append({"POS": "AUX", "OP": "*"})
-                    heur_sent3.append({"POS": "AUX", "OP": "*"})
+                    heur_sent.append({"POS": "AUX", "OP": "+"})
+                    heur_sent2.append({"POS": "AUX", "OP": "+"})
+                    heur_sent3.append({"POS": "AUX", "OP": "+"})
                 elif token.text == "link":
-                    heur_sent.append({"POS": "X", "OP": "*"})
-                    heur_sent2.append({"POS": "X", "OP": "*"})
-                    heur_sent3.append({"POS": "X", "OP": "*"})
+                    heur_sent.append({"POS": "X", "OP": "+"})
+                    heur_sent2.append({"POS": "X", "OP": "+"})
+                    heur_sent3.append({"POS": "X", "OP": "+"})
                 elif token.text == "date":
-                    heur_sent.append({"POS": "NUM", "OP": "*"})
-                    heur_sent2.append({"POS": "NUM", "OP": "*"})
-                    heur_sent3.append({"POS": "NUM", "OP": "*"})
+                    heur_sent.append({"POS": "NUM", "OP": "+"})
+                    heur_sent2.append({"POS": "NUM", "OP": "+"})
+                    heur_sent3.append({"POS": "NUM", "OP": "+"})
                 elif token.pos_ not in pos_lst:
-                    heur_sent.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "*"})
-                    heur_sent2.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "*"})
-                    heur_sent3.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "*"})
+                    heur_sent.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "+"})
+                    heur_sent2.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "+"})
+                    heur_sent3.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "+"})
             else:
                 if token.text == "verb":
-                    heur_sent.append({"POS": "VERB", "OP": "*"})
+                    heur_sent.append({"POS": "VERB", "OP": "+"})
                 elif token.text == "modal":
-                    heur_sent.append({"POS": "AUX", "OP": "*"})
+                    heur_sent.append({"POS": "AUX", "OP": "+"})
                 elif token.text == "link":
-                    heur_sent.append({"POS": "X", "OP": "*"})
+                    heur_sent.append({"POS": "X", "OP": "+"})
                 elif token.text == "date":
-                    heur_sent.append({"POS": "NUM", "OP": "*"})
+                    heur_sent.append({"POS": "NUM", "OP": "+"})
                 elif token.pos_ not in pos_lst:
-                    heur_sent.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "*"})
+                    heur_sent.append({"LEMMA": token.lemma_, "POS": token.pos_, "OP": "+"})
         if heur_sent2:
             heur_list.extend([heur_sent, heur_sent2, heur_sent3])
         else:
