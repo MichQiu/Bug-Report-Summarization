e5ef314fd6d0c4a24984a6407e927c07df3ce467
MichQiu <qiu_michael@hotmail.com>
2020-09-24 13:44:30 +0100

    Changed lower case default from true to false

--- src/create_pretraining_data_h.py
+++ src/create_pretraining_data_h.py
@@ -483,8 +483,8 @@ def main():
                         help="Probability to create a sequence shorter than maximum sequence length")
 
     parser.add_argument("--do_lower_case",
-                        action='store_true',
-                        default=True,
+                        action='store_false',
+                        default=False,
                         help="Whether to lower case the input text. True for uncased models, False for cased models.")
     parser.add_argument('--random_seed',
                         type=int,
