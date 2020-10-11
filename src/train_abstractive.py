d5e089c545a3955abe65d1f01b47902cde6d5e8d
MichQiu <qiu_michael@hotmail.com>
2020-10-11 13:34:08 +0100

    Replaced standard BERT vocab tokenizer with Bugzilla vocab Replaced special sequence position tokens

--- src/train_abstractive.py
+++ src/train_abstractive.py
@@ -12,7 +12,7 @@ import signal
 import time
 
 import torch
-from pytorch_transformers import BertTokenizer
+from tokenization import BertTokenizer
 
 import distributed
 from models import data_loader, model_builder
@@ -188,9 +188,9 @@ def validate(args, device_id, pt, step):
                                         shuffle=False, is_test=False)
 
     # get tokenizer and symbols for special tokens
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-    symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
-               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+    tokenizer = BertTokenizer(args.vocab, do_lower_case=False)
+    symbols = {'BOS': tokenizer.vocab['上'], 'EOS': tokenizer.vocab['下'],
+               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['中']}
 
     valid_loss = abs_loss(model.generator, symbols, model.vocab_size, train=False, device=device)
 
@@ -221,9 +221,9 @@ def test_abs(args, device_id, pt, step):
     test_iter = data_loader.Dataloader(args, load_dataset(args, 'test', shuffle=False),
                                        args.test_batch_size, device,
                                        shuffle=False, is_test=True)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-    symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
-               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+    tokenizer = BertTokenizer(args.vocab, do_lower_case=False)
+    symbols = {'BOS': tokenizer.vocab['上'], 'EOS': tokenizer.vocab['下'],
+               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['中']}
     predictor = build_predictor(args, tokenizer, symbols, model, logger) # build predictor for test set
     predictor.translate(test_iter, step)
 
@@ -250,9 +250,9 @@ def test_text_abs(args, device_id, pt, step):
     test_iter = data_loader.Dataloader(args, load_dataset(args, 'test', shuffle=False),
                                        args.test_batch_size, device,
                                        shuffle=False, is_test=True)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-    symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
-               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+    tokenizer = BertTokenizer(args.vocab, do_lower_case=False)
+    symbols = {'BOS': tokenizer.vocab['上'], 'EOS': tokenizer.vocab['下'],
+               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['中']}
     predictor = build_predictor(args, tokenizer, symbols, model, logger)
     predictor.translate(test_iter, step)
 
@@ -327,9 +327,9 @@ def train_abs_single(args, device_id):
 
     logger.info(model)
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-    symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
-               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+    tokenizer = BertTokenizer(args.vocab, do_lower_case=False)
+    symbols = {'BOS': tokenizer.vocab['上'], 'EOS': tokenizer.vocab['下'],
+               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['中']}
 
     # use abstractive summarization loss
     train_loss = abs_loss(model.generator, symbols, model.vocab_size, device, train=True,
